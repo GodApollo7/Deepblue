f63379e881a5e33cf84e1e3464673fc9559dae7b
GodApollo7 <110694879+GodApollo7@users.noreply.github.com>
2024-02-16 08:11:49 +0000

    Initial piece of code with Key (Esmail) and chromadb

--- openai.py
+++ openai.py
@@ -1,1 +1,70 @@
+import chromadb
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.question_answering import load_qa_chain
+from langchain.chat_models import ChatOpenAI
+from langchain.vectorstores import Chroma
+
+
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
+
+OPENAI_API_KEY="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"
+
+
+def load_chunk_persist_pdf() -> Chroma:
+    pdf_folder_path = "/workspaces/Deepblue/"
+    documents = []
+    for file in os.listdir(pdf_folder_path):
+        if file.endswith('.pdf'):
+            pdf_path = os.path.join(pdf_folder_path, file)
+            loader = PyPDFLoader(pdf_path)
+            documents.extend(loader.load())
+    text_splitter = CharacterTextSplitter(chunk_size=10000, chunk_overlap=100)
+    chunked_documents = text_splitter.split_documents(documents)
+    client = chromadb.Client()
+    if client.list_collections():
+        consent_collection = client.create_collection("data")
+    else:
+        print("Collection already exists")
+    vectordb = Chroma.from_documents(
+        documents=chunked_documents,
+        embedding=OpenAIEmbeddings(),
+        persist_directory="/workspaces/Deepblue/Something"
+    )
+    vectordb.persist()
+    return vectordb
+
+
+def create_agent_chain():
+    model_name = "gpt-3.5-turbo"
+    llm = ChatOpenAI(model_name=model_name)
+    chain = load_qa_chain(llm, chain_type="stuff")
+    return chain
+
+
+def get_llm_response(query):
+    vectordb = load_chunk_persist_pdf()
+    chain = create_agent_chain()
+    matching_docs = vectordb.similarity_search(query)
+    answer = chain.run(input_documents=matching_docs, question=query)
+    return answer
+
+
+# Streamlit UI
+# ===============
+st.set_page_config(page_title="Doc Searcher", page_icon=":robot:")
+st.header("Query PDF Source")
+
+form_input = st.text_input('Enter Query')
+submit = st.button("Generate")
+
+if submit:
+    st.write(get_llm_response(form_input))
 
